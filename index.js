fe9f8a6856a3b4bcf0942d54b1f885a39a3d3230
Peter Bacenet <peterbacenet@icloud.com>
2021-08-22 02:51:22 -0400

    commit tree 73d99f67d5baff124f13641d8400a6efadd46879

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
 
-// Write your code here
+let num2 = 2
+
+let multiply = num1*num2
+
+let random = Math.ceil(Math.random() * 10)
+
+let num3 = 20
+
+let num4 = 5
+
+let mod = num3/num4
+
+let max = (1,2,3,20)
